c19db68d310a5b84b6ceab151631a4ecec08583c
wanglin <wanglin20130410@126.com>
2016-09-11 20:17:41 +0800

    WOOL-0000: init

---
+++ src/main/java/com/wool/hermes/Demo.java
@@ -0,0 +1,10 @@
+package com.wool.hermes;
+
+/**
+ * Created by wanglin on 16-9-11.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
