51d927504c1743e3f64e1ba633bfa793a734f31b
wanglin <wanglin20130410@126.com>
2016-10-05 14:56:00 +0800

    WOOL-0005: modify Application config and ds properties

--- src/main/java/com/wool/hermes/Application.java
+++ src/main/java/com/wool/hermes/Application.java
@@ -1,9 +1,13 @@
 package com.wool.hermes;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
@@ -14,6 +18,8 @@ import org.springframework.web.filter.CharacterEncodingFilter;
  */
 @SpringBootApplication
 @Configuration
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
+@EnableCaching
 @ImportResource({"classpath*:spring/spring.xml"})
 public class Application extends SpringBootServletInitializer {
 
@@ -27,7 +33,7 @@ public class Application extends SpringBootServletInitializer {
     }
 
     /**
-     * add character filter
+     * character filter
      * @return
      */
     @Bean
